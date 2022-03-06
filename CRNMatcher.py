9ec4fe65370a419fd84c236c1b96fbcdb55734c0
Se Jin Park <zcabsjp@ucl.ac.uk>
2022-03-06 22:07:39 +0900

    Add cleanCRN Method and Excel Export Functionality

--- CRNMatcher.py
+++ CRNMatcher.py
@@ -1,23 +1,36 @@
-from queue import Empty
 import pandas as pd
 
-excel1 = pd.read_excel('./assets/input1.xlsx')
+
+def cleanCRNs(excel_df: pd.DataFrame):
+    crnList = []
+
+    for index, row in excel_df.iterrows():
+        crnList.append(row["사업자등록번호"].strip())
+
+    excel_df["사업자등록번호"] = crnList
+    return excel_df
+
+
+excel1 = cleanCRNs(pd.read_excel('./assets/input1.xlsx'))
+excel2 = cleanCRNs(pd.read_excel('./assets/input2.xlsx'))
+
+print(excel1)
+print(excel2)
 
 nameList = []
 addressList = []
 
-excel2 = pd.read_excel('./assets/input2.xlsx')
-
 for crn in excel1["사업자등록번호"]:
-    key = (str)(crn.strip())
-    targetRow = excel2.loc[excel2["사업자등록번호"] == key]
+    targetRow = excel2.loc[excel2["사업자등록번호"] == crn]
 
     if targetRow.empty:
         nameList.append("일치하는 사업자 없음")
         addressList.append("일치하는 사업자 없음")
     else:
-        nameList.append(targetRow.iloc[0]["대표자명"])
-        addressList.append(targetRow.iloc[0]["주소"])
+        nameList.append(targetRow.iloc[0]["대표자명"].strip())
+        addressList.append(targetRow.iloc[0]["주소"].strip())
 
 excel1.insert(len(excel1.columns), "대표자명", nameList, True)
 excel1.insert(len(excel1.columns), "주소", addressList, True)
+
+excel1.to_excel('./assets/output.xlsx')
